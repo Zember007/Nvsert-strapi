13ec4aa0f40a7a965f2f6d2649d5096adfb4c75c
Georgii Borisov <goraegorgora@mail.ru>
2025-08-12 14:55:06 +0400

    fix

--- src/api/service/controllers/service.ts
+++ src/api/service/controllers/service.ts
@@ -4,6 +4,12 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::service.service');
-
-
+export default factories.createCoreController('api::service.service', ({ strapi }) => ({
+    async find(ctx) {
+      ctx.query = {
+        ...ctx.query,
+        sort: ['order:asc']
+      };
+      return await super.find(ctx);
+    }
+  }));
