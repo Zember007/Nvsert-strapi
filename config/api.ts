42b73010c3a9082e860f33caefcc90b2a656bb83
Georgii Borisov <goraegorgora@mail.ru>
2025-10-16 13:31:39 +0400

    add

--- config/api.ts
+++ config/api.ts
@@ -3,6 +3,6 @@ export default {
     defaultLimit: 25,
     maxLimit: 100,
     withCount: true,
-    defaultPopulate: ['category', 'img', 'documents']
+    defaultPopulate: ['category', 'img', 'documents', 'content', 'content.image', 'cta', 'seo']
   },
 };
--- src/api/service/controllers/service.ts
+++ src/api/service/controllers/service.ts
@@ -8,8 +8,39 @@ export default factories.createCoreController('api::service.service', ({ strapi 
     async find(ctx) {
       ctx.query = {
         ...ctx.query,
-        sort: ['order:asc']
+        sort: ['order:asc'],
+        populate: {
+          category: true,
+          img: true,
+          documents: true,
+          content: {
+            populate: {
+              image: true
+            }
+          },
+          cta: true,
+          seo: true
+        }
       };
       return await super.find(ctx);
+    },
+    
+    async findOne(ctx) {
+      ctx.query = {
+        ...ctx.query,
+        populate: {
+          category: true,
+          img: true,
+          documents: true,
+          content: {
+            populate: {
+              image: true
+            }
+          },
+          cta: true,
+          seo: true
+        }
+      };
+      return await super.findOne(ctx);
     }
   }));
