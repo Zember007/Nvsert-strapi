184ad64a7b8913edd0b92c5b7826301c73359b00
Georgii Borisov <goraegorgora@mail.ru>
2025-10-17 16:45:53 +0400

    fix

--- src/api/about/controllers/about.ts
+++ src/api/about/controllers/about.ts
@@ -4,4 +4,34 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::about.about');
+export default factories.createCoreController('api::about.about', ({ strapi }) => ({
+  async find(ctx) {
+    ctx.query = {
+      ...ctx.query,
+      populate: {
+        content: {
+          populate: {
+            image: true
+          }
+        },
+        seo: true
+      }
+    };
+    return await super.find(ctx);
+  },
+  
+  async findOne(ctx) {
+    ctx.query = {
+      ...ctx.query,
+      populate: {
+        content: {
+          populate: {
+            image: true
+          }
+        },
+        seo: true
+      }
+    };
+    return await super.findOne(ctx);
+  }
+}));
