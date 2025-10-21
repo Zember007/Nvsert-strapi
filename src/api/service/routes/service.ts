92807478809ec03d9b9d78762294388c217fc288
Georgii Borisov <goraegorgora@mail.ru>
2025-10-21 12:24:51 +0400

    fix

--- src/api/service/routes/service.ts
+++ src/api/service/routes/service.ts
@@ -4,20 +4,6 @@
 
 import { factories } from '@strapi/strapi';
 
-const defaultRouter = factories.createCoreRouter('api::service.service');
-
-export default {
-  routes: [
-    {
-      method: 'GET',
-      path: '/services/slug/:slug',
-      handler: 'service.findBySlug',
-      config: {
-        auth: false,
-      },
-    },
-    ...(typeof defaultRouter.routes === 'function' ? defaultRouter.routes() : defaultRouter.routes),
-  ],
-};
+export default factories.createCoreRouter('api::service.service');
 
 
