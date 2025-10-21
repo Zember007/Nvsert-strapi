1b203f74038a140b9a7cb4d66307aedd32834f12
Georgii Borisov <goraegorgora@mail.ru>
2025-10-21 12:30:33 +0400

    fix

---
+++ src/api/service/routes/custom-service.ts
@@ -0,0 +1,13 @@
+export default {
+    routes: [
+      {
+        method: 'GET',
+        path: '/services/slug/:slug',
+        handler: 'service.findBySlug',
+        config: {
+          auth: false, // можно включить при необходимости
+        },
+      },
+    ],
+  };
+  
