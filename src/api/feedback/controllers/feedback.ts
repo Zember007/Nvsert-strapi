82431da66ae4b730e5079e50c8af2bc1138f4579
Georgii Borisov <goraegorgora@mail.ru>
2025-10-27 15:45:22 +0400

    fix

--- src/api/feedback/controllers/feedback.ts
+++ src/api/feedback/controllers/feedback.ts
@@ -6,11 +6,15 @@ import { factories } from '@strapi/strapi'
 
 export default factories.createCoreController('api::feedback.feedback', ({ strapi }) => ({
     async find(ctx) {
-      ctx.query = {
-        ...ctx.query,
-        sort: ['order:asc'],
-        populate: ['*'],
-      };
-      return await super.find(ctx);
+        ctx.query = {
+            ...ctx.query,
+            sort: ['order:asc'],
+            populate: ['*'],
+            pagination: {
+                page: 1,
+                pageSize: 1000
+            },
+        };
+        return await super.find(ctx);
     }
-  }));
+}));
