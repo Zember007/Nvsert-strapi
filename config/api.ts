c4cf7f9db3805106e3da8838a8d54ae5664b2efb
Georgii Borisov <goraegorgora@mail.ru>
2025-12-23 14:18:39 +0400

    edit okpd2

--- config/api.ts
+++ config/api.ts
@@ -1,7 +1,7 @@
 export default {
   rest: {
     defaultLimit: 25,
-    maxLimit: 100,
+    maxLimit: 200000,
     withCount: true,
     defaultPopulate: ['category', 'img', 'documents', 'content', 'content.image', 'cta', 'seo']
   },
