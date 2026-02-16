ac8baeb618e62ed47dc039b48dd04d945794a5c1
Georgii Borisov <goraegorgora1306@gmail.com>
2026-02-16 18:06:14 +0400

    fix

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  // Обязательно для корректной авторизации админки за reverse proxy (сессия/куки)
+  host: '0.0.0.0',
+  port: 1337,
+  url: 'https://test11-admin.audiosector.ru',
   proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
