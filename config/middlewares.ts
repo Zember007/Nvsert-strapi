f54cb6926ec5d12ef127ea325320ee4d982d683f
Georgii Borisov <goraegorgora1306@gmail.com>
2026-02-17 14:50:02 +0400

    fix api

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -12,7 +12,17 @@ module.exports = [
     }
   },
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'https://test11.audiosector.ru',
+        'http://test11.audiosector.ru'
+      ],
+      headers: '*',
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
