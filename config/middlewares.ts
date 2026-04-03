adb90fdee807e292450fac55ad6d3a711cd56193
Georgii Borisov <goraegorgora1306@gmail.com>
2026-04-03 18:04:12 +0400

    update

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -26,6 +26,8 @@ export default ({ env }) => [
         'http://www.test11.audiosector.ru',
         'http://localhost:3000',
         'http://127.0.0.1:3000',
+        'http://test11-admin.audiosector.ru',
+
       ],
       headers: '*',
       credentials: true,
